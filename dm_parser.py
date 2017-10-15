4054fe473f7c979499abcf59372fd371be05e5f2
Dennis Mesina <dennismesina@Denniss-MacBook-Pro.local>
2017-10-15 13:59:09 -0700

    Building parser and tester -dennis

--- dm_parser.py
+++ dm_parser.py
@@ -1,10 +1,34 @@
 '''This parser will look for KV items and store them in a dictionary'''
+import re
 
 class Parser(object):
 	elements = {}
 	file_name = ""
+	file = None
 
 	def __init__(self, file_name):
 		self.file_name = file_name
+		self.open()
+		self.list = self.get_usable_lines()
+		self.create_dictionary(self.list)
+		self.close()
 
-	
+	def open():
+		self.file = open(self.file_name, 'r')
+
+	def close():
+		self.file.close()
+
+	def get_usable_lines():
+		usable_lines = []
+		all_lines = self.file.readlines()
+		for line in all_lines:
+			if line[0] == '#':
+				usable_lines.append(line)
+		return usable_lines
+
+	def create_dictionary(line_list):
+		for line in line_list:
+			key = re.search('#\s*(.*)?(?=\s=\s)', line)
+			value = re.search('(?<=\s=\s)(.*)?\n', line)
+			elements[key.group(1)] = value.group(0)
--- parser_tester.py
+++ parser_tester.py
@@ -1,1 +1,5 @@
 '''This tester will test the dm_parser.py'''
+
+import dm_parser
+
+file_parser = dm_parser.Parser()

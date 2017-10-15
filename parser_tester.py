2c3aaef92a7e4bcdbb3c0f62a4eac7598bb7596a
Dennis Mesina <dennismesina@Denniss-MacBook-Pro.local>
2017-10-15 14:38:07 -0700

    testing spliting an entry into a list -dennis

--- parser_tester.py
+++ parser_tester.py
@@ -7,3 +7,7 @@ my_parser = dm_parser.Parser("test.dm")
 file_contents = my_parser.elements
 
 print(file_contents)
+
+file_contents['list'] = file_contents['list'].split()
+
+print(file_contents)
--- test.dm
+++ test.dm
@@ -2,6 +2,7 @@
 #	secondword = is
 #	third word = a
 #	fourth word = test
+#	list = 123.123.123.1 123.123.123.2 123.123.123.3 123.123.123.4 123.123.123.5
 
 These lines should
 not be part of the
